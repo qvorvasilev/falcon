d64a65416d233beb6642f590ec305d34e73661e1
Явор Василев <qvor.vasilev@gmail.com>
2018-08-20 22:03:57 +0300

    WIP - search form

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -2,6 +2,18 @@ import jQuery from "./vendor/jquery.min.js";
 
 
 jQuery(document).ready(function($) {
+	
+	/* Shows clear search button */
+	$('#search').on('keyup', function(event) {
+		console.log(1);
+		if ($(this).val()) {
+			$(this).siblings('button').show('fast');
+		} else {
+			$(this).siblings('button').hide('fast');
+		}
+	});
+
+
 	$('.disabled').click(function(event) {
 		console.log(1);
 		return false;
