d0813b7e565272958a572c7bfbc34b3d1fec78e5
Yavor Vasilev <qvor.vasilev@gmail.com>
2018-08-24 00:12:54 +0300

    parallax reworked. Elements are now created with CSS. JS logic ammended properly apply position and shadow.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -30,52 +30,82 @@ jQuery(document).ready(function($) {
 
 	// Parallax
 
-	// function name (argument) {
+	var sky = $('.sky');
 	var sun = $('.parallax-img.sun');
 	var surface = $('.surface');
 	var pyramidLeft = $('.pyramid-left');
 	var pyramidRight = $('.pyramid-right');
-		// var sun = $('.sun');
-	// }
+
+  	// Distance from parralax element to top 
+  	// minus the section title height
+    var distanceFromParallax = $('.parallax').offset().top - 35;
+  	var scrollDistance = $(window).scrollTop();
+  	var scrollPercent = 0;
+  	
+  	function parallaxEffect() {
+		scrollDistance = $(window).scrollTop(),
+	  	scrollPercent = Math.round((scrollDistance / distanceFromParallax) * 100);
+
+	  	if(scrollPercent > 100) {
+	  		scrollPercent = 100;
+	  	}
+	  
+
+	  	var sunPos = Math.round(50 + scrollPercent*0.65) + 'px'; //because sun needs to travel exactly 65px distance
+	  	var pyramidPosLeft = Math.round(15 - scrollPercent*0.2) + 'px';
+	  	var pyramidPosRight = Math.round(-20 - scrollPercent*0.2) + 'px';
+	  	var surfacePos = Math.round(0 - scrollPercent*0.2) + 'px';
+	  	var skyPos = Math.round(0 - scrollPercent*5.1) + 'px';
+	  	console.log(scrollPercent);
+
+	  	var sunShadow = sunBoxShadow(0, 0, 20, 5, scrollPercent);
+	  	// var pyramidShadow = pyramidBoxShadow(2, 2, 10, 2, scrollPercent);
+	  	// console.log(pyramidShadow);
 
 
 
-  	var s = $(window).scrollTop(),
-      	d = $(document).height(),
-      	c = $(window).height(),
-      	scrollPercent = 0;
+		sun.css('box-shadow', '0px 0px ' + sunShadow.blur + 'px ' + sunShadow.spread + 'px #fc00d2');
+
+		// pyramidLeft.css('box-shadow', pyramidShadow.horizontal+'px '+ pyramidShadow.vertical+'px ' + pyramidShadow.blur + 'px ' + pyramidShadow.spread + 'px rgba(8, 10, 101, .75)');
+		// pyramidRight.css('box-shadow', pyramidShadow.horizontal+'px '+ pyramidShadow.vertical+'px ' + pyramidShadow.blur + 'px ' + pyramidShadow.spread + 'px rgba(8, 10, 101, .75)');
+
+		// pyramidLeft.css('box-shadow', '0px 0px '  + (5 - (1/scrollPercent)*100) + 'px ' + (5 - (1/scrollPercent)*100) + 'px #080a65');
+
+
+	  	sky.css('top', skyPos);
+	  	sun.css('bottom', sunPos);
+	  	pyramidLeft.css('bottom', pyramidPosLeft);
+	  	pyramidRight.css('bottom', pyramidPosRight);
+	  	surface.css('bottom', surfacePos);
+  	}
+
+  	function sunBoxShadow (h, v, b, s, percentage) {
+  		var boxObj = {};
+
+	  	boxObj.horizontal = Math.ceil(h - (1/percentage)*100);
+	  	boxObj.vertical = Math.ceil(v - (1/percentage)*100);
+	  	boxObj.blur = Math.ceil(b - (1/percentage)*100);
+	  	boxObj.spread = Math.ceil(s - (1/percentage)*100);
+
+	  	return boxObj;
+  	}
+  	function pyramidBoxShadow (h, v, b, s, percentage) {
+  		var boxObj = {};
+
+	  	boxObj.horizontal = Math.ceil(h+1 - (h*percentage)/100);
+	  	boxObj.vertical = Math.ceil(v+1 - (h*percentage)/100);
+	  	boxObj.blur = Math.ceil(b+1 - (b/100)*percentage);
+	  	boxObj.spread = Math.ceil(s+1 - (s/100)*percentage);
+
+	  	return boxObj;
+  	}
+
+
+	parallaxEffect();
 
 	$(window).on('scroll', function(){
-		s = $(window).scrollTop(),
-      	d = $(document).height(),
-      	c = $(window).height();
-      	// console.log(d);
-
-	  	scrollPercent = (s / (d - c)) * 100;
-	  
-	  	// console.clear();
-	  	// console.log(Math.round(scrollPercent));
-
-	  	// sun.css('display', 'none');
-	  	var sunPos = Math.round(370 - scrollPercent*2.6) + 'px';
-	  	var pyramidPosLeft = Math.round(342 - scrollPercent*1.85) + 'px';
-	  	var pyramidPosRight = Math.round(289 - scrollPercent*1.85) + 'px';
-	  	var surfacePos = Math.round(410 - scrollPercent*1.85) + 'px';
-	  	console.log(surfacePos);
-
-
-	  	sun.css('background-position-y', sunPos);
-	  	pyramidLeft.css('background-position-y', pyramidPosLeft);
-	  	pyramidRight.css('background-position-y', pyramidPosRight);
-	  	surface.css('background-position-y', surfacePos);
-
+		parallaxEffect();
 	})
-
-
-
-
-
-
 });		
 
 
